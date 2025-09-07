320c4016d5c2f89a934da4f23ee7eb9dc06944e6
PreethamSG56 <144652053+PreethamSG56@users.noreply.github.com>
2025-09-07 14:32:46 +0530

    Update streamlit_app.py
    connected canged from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,6 +10,9 @@ st.write(
     Select fruit and customize your smoothie.
     """
 )
+
+cnx = st.connection("Snowflake")
+session = cnx.session()
 
 # Input for smoothie name
 name_on_order = st.text_input("Name of Smoothie:")
